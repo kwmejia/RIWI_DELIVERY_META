bb987c99969407417eb16f1f155882db800e1889
danielsm <danielsanchezm92@gmail.com>
2024-03-13 09:04:40 -0500

    created user class

---
+++ src/entity/User.java
@@ -0,0 +1,78 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class User extends Account{
+
+    private String name;
+
+    private String phone;
+
+    private ArrayList<Order> history;
+
+    private double rating;
+
+    private int id;
+
+    public User(String name, String phone, double rating, int id, String username, String password, String rol) {
+        this.name = name;
+        this.phone = phone;
+        this.history = new ArrayList<Order>();
+        this.rating = rating;
+        this.id = id;
+        super(username,password,rol);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Order> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Order> history) {
+        this.history = history;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return super().toString() + "User{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", history=" + history +
+                ", rating=" + rating +
+                ", id=" + id +
+                '}';
+    }
+}
+
+
