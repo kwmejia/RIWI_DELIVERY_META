44104b7c336b5102e1eea8a9bde5dd632bc7ee05
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-03-13 09:12:51 -0500

    principal menu

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,43 @@
+import javax.swing.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+
+        int option = 0;
+        do{
+
+            try {
+                option = Integer.parseInt(JOptionPane.showInputDialog(null,"""
+                        PRINCIPAL MENU
+                            1. Login
+                            2. Sign up
+                            3. Exit
+                        """));
+
+                switch (option){
+                    case 1:
+                        JOptionPane.showMessageDialog(null,"Login");
+                        break;
+
+                    case 2:
+                        JOptionPane.showMessageDialog(null,"Sign up");
+                        break;
+
+                    case 3:
+                        JOptionPane.showMessageDialog(null,"leaving");
+                        break;
+
+                }
+
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null,"Invalid option");
+            }
+
+        }while (option!=3);
+
+
     }
 }
